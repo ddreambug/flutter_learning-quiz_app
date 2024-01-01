b4e5d329058ab18519d9b90409436679df1ac722
Dest <86515936+ddreambug@users.noreply.github.com>
2024-01-01 23:57:20 +0700

    update comment

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -4,3 +4,5 @@ class QuizQuestion {
   final String text;
   final List<String> answers;
 }
+
+// nanti buat semua integrasi dari question.dart kedalam quiz question kemudinan untuk ditampilkan ke question screen.dart
