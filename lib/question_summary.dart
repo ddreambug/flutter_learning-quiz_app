94711eba81cb63ad3070383ee6cfb25d51b47c46
Dest <86515936+ddreambug@users.noreply.github.com>
2024-01-07 00:00:19 +0700

    adding SingleChildScrollView to make fixed summary size scrollable

--- lib/question_summary.dart
+++ lib/question_summary.dart
@@ -7,24 +7,26 @@ class QuestionSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 450,
-      child: Column(
-          children: summaryData.map((data) {
-        return Row(children: [
-          Text(((data['question_index'] as int) + 1).toString()),
-          Expanded(
-            child: Column(
-              children: [
-                Text((data['question'] as String)),
-                const SizedBox(height: 5),
-                Text((data['user_answer'] as String)),
-                const SizedBox(height: 5),
-                Text((data['correct_answer'] as String)),
-              ],
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+            children: summaryData.map((data) {
+          return Row(children: [
+            Text(((data['question_index'] as int) + 1).toString()),
+            Expanded(
+              child: Column(
+                children: [
+                  Text((data['question'] as String)),
+                  const SizedBox(height: 5),
+                  Text((data['user_answer'] as String)),
+                  const SizedBox(height: 5),
+                  Text((data['correct_answer'] as String)),
+                ],
+              ),
             ),
-          ),
-        ]);
-      }).toList()),
+          ]);
+        }).toList()),
+      ),
     );
   }
 }
