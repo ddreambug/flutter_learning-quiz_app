8df4c40f6c4f0a731ebb2f2a6fd47d1970a6fdaa
Dest <86515936+ddreambug@users.noreply.github.com>
2024-01-07 22:27:26 +0700

    complete app

--- lib/question_summary.dart
+++ lib/question_summary.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/models/styled_text.dart';
 
 class QuestionSummary extends StatelessWidget {
+  //dipanggil di resultscreen
   const QuestionSummary({super.key, required this.summaryData});
 
   final List<Map<String, Object>> summaryData;
 
   @override
   Widget build(BuildContext context) {
+    listCheck(int dataIndex) {
+      final splitSummary = summaryData[dataIndex];
+
+      bool isRightAnswer =
+          splitSummary['correct_answer'] == splitSummary['user_answer'];
+
+      return isRightAnswer;
+    }
+
     return SizedBox(
       height: 300,
       child: SingleChildScrollView(
@@ -15,8 +25,11 @@ class QuestionSummary extends StatelessWidget {
             children: summaryData.map((data) {
           return Row(children: [
             CircleAvatar(
-                // gimana cara setting warna circleavatar sesuai jawaban yang benar
-                child: Text(((data['question_index'] as int) + 1).toString())),
+              backgroundColor: (listCheck(data['question_index'] as int)
+                  ? const Color.fromARGB(255, 154, 255, 152)
+                  : const Color.fromARGB(255, 199, 64, 64)),
+              child: Text(((data['question_index'] as int) + 1).toString()),
+            ),
             const SizedBox(width: 20),
             Expanded(
               child: Column(
@@ -32,7 +45,7 @@ class QuestionSummary extends StatelessWidget {
                   StyledText(
                     text: (data['user_answer'] as String),
                     fontsize: 12,
-                    fontColor: Color.fromARGB(255, 216, 186, 227),
+                    fontColor: const Color.fromARGB(255, 216, 186, 227),
                   ),
                   const SizedBox(height: 2),
                   StyledText(
